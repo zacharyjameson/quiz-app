52119bc71a8ec8336ea86d84ce9acc06e4bc68b5
Zack Jameson <zacharymjameson@gmail.com>
2021-01-11 08:42:00 -0500

    first function and header for html

--- app.js
+++ app.js
@@ -1,34 +1,73 @@
 /**
  * Example store structure
  */
-const store = {
-  // 5 or more questions are required
-  questions: [
-    {
-      question: 'What color is broccoli?',
-      answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
+const STORE = [
+  {
+    question: 'Where was Gondor when the Westfold fell?',
+    answers: [
+      'Cair Andros',
+      'Osgiliath',
+      'Ravenhill',
+      'Dunland',
+      'Angmar'
       ],
-      correctAnswer: 'green'
-    },
-    {
-      question: 'What is the current year?',
-      answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
-      ],
-      correctAnswer: '2019'
-    }
-  ],
-  quizStarted: false,
-  questionNumber: 0,
-  score: 0
-};
+    correctAnswer: 'Osgiliath' || 'Cair Andros',
+  },
+  {
+    question: 'One does not simply walk into which realm?',
+    answers: [
+      'Mordor',
+      'Rohan',
+      'Hobbiton',
+      'Gondor',
+      'Iron Hills'
+    ],
+    correctAnswer: 'Mordor',
+  },
+  {
+    question: 'Gimli won the battle of killing orcs by what score at Helm\'s Deep?',
+    answers: [
+      '43-42',
+      '37-16',
+      '28-3',
+      'It was a tie',
+      'Gimlie actually lost'
+    ],
+    correctAnswer: '43-42',
+  },
+  {
+    question: 'In Fangorn forest, what do Merry and Pippen drink in order to inexplicably grow taller?',
+    answers: [
+      'Water',
+      'Mead',
+      'Ent-draught',
+      'Ale',
+      'Orc-draught'
+    ],
+    correctAnswer: 'Ent-draught',
+  },
+  {
+    question: 'What is the password which need be spoken in order to enter the Mines of Moria through the Doors of Durin?',
+    answers: [
+      'Open Sesame',
+      'Annon Edhellen, edro hi ammen!',
+      'Fennas Nogothrim, lasto beth lammen',
+      'Mellon',
+      'Ando Eldarinwa…a lasta quettanya, Fenda Casarinwa'
+    ],
+    correctAnswer: 'Mellon',
+  },
+  {
+    question: 'Viggo Mortensen (Aragorn) actually breaks his toe after kicking the helmet of a fallen Uruk-hai after believing what to be true?',
+    answers: [
+      'They had run out of Elvish bread',
+      'His reservation at the inn of the Prancing Pony was mistakenly cancelled',
+      'He believed to have failed Merry and Pippen',
+      'Legolas had led them down the wrong path in pursuit of Merry and Pippen across the Gap of Rohan'
+    ],
+    correctAnswer: 'He believed to have failed Merry and Pippen',
+  }
+];
 
 /**
  * 
@@ -48,6 +87,18 @@ const store = {
 /********** TEMPLATE GENERATION FUNCTIONS **********/
 
 // These functions return HTML templates
+
+function generateWelcomeScreen () {
+  return `
+  <div class="startQuizScreen">
+    <form>
+        <h2>The Not By Any Means Complete or Exhaustive Quiz On Middle Earth</h2>
+        <h3>Click the button below when you're ready for an adventure</h3>
+        <button type="button" class="startQuiz" autofocus>I'm going on an adventure!</button>
+      </form>
+    </div> 
+    `;
+}
 
 /********** RENDER FUNCTION(S) **********/
 
