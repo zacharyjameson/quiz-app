635caaf32c04b155ef7f14d278da1243567034e0
Zack Jameson <zacharymjameson@gmail.com>
2021-01-12 20:00:57 -0500

    functioning js

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const STORE = {
   questions: [
     {
       question: "Where was Gondor when the Westfold fell?",
-      answers: ["Cair Andros", "Osgiliath", "Ravenhill", "Dunland", "Angmar"],
-      correctAnswer: "Osgiliath" || "Cair Andros",
+      answers: ["The Shire", "Osgiliath", "Ravenhill", "Dunland", "Angmar"],
+      correctAnswer: "Osgiliath"
     },
     {
       question: "One does not simply walk into which realm?",
       answers: ["Mordor", "Rohan", "Hobbiton", "Gondor", "Iron Hills"],
-      correctAnswer: "Mordor",
+      correctAnswer: "Mordor"
     },
     {
       question:
@@ -21,15 +21,15 @@ const STORE = {
         "37-16",
         "28-3",
         "It was a tie",
-        "Gimlie actually lost",
+        "Gimlie actually lost"
       ],
-      correctAnswer: "43-42",
+      correctAnswer: "43-42"
     },
     {
       question:
         "In Fangorn forest, what do Merry and Pippen drink in order to inexplicably grow taller?",
       answers: ["Water", "Mead", "Ent-draught", "Ale", "Orc-draught"],
-      correctAnswer: "Ent-draught",
+      correctAnswer: "Ent-draught"
     },
     {
       question:
@@ -39,9 +39,9 @@ const STORE = {
         "Annon Edhellen, edro hi ammen!",
         "Fennas Nogothrim, lasto beth lammen",
         "Mellon",
-        "Ando Eldarinwa…a lasta quettanya, Fenda Casarinwa",
+        "Ando Eldarinwa…a lasta quettanya, Fenda Casarinwa"
       ],
-      correctAnswer: "Mellon",
+      correctAnswer: "Mellon"
     },
     {
       question:
@@ -52,7 +52,7 @@ const STORE = {
         "He believed to have failed Merry and Pippen",
         "Legolas had led them down the wrong path in pursuit of Merry and Pippen across the Gap of Rohan",
       ],
-      correctAnswer: "He believed to have failed Merry and Pippen",
+      correctAnswer: "He believed to have failed Merry and Pippen"
     },
   ],
   quizStarted: false,
@@ -80,7 +80,7 @@ const STORE = {
 function generateQuizStartScreen() {
   return `
     <div class="startScreen">
-      <h2>Fool of a Took! Click the button below to test your knowledge.</h2>
+      <h3>Fool of a Took! Click the button below to test your knowledge.</h3>
       <button type="button" id="startButton">Begin Quiz</button>
       </div>
   `;
@@ -90,26 +90,24 @@ function generateQuestionNumberAndScore() {
   return `
     <ul class="question-score">
       <li id="question-number">
-        Question Number: ${STORE.currentQuestion + 1}/${STORE.questions.length};
+        Question Number: ${STORE.currentQuestion + 1}/${STORE.questions.length}
       </li>
       <li id="score">
-        Score: ${STORE.score}/${STORE.questions.length};
+        Score: ${STORE.score}/${STORE.questions.length}
       </li>
-      </ul>
+    </ul>
   `;
 }
 
 function generateAnswers() {
-  const answersArray = STORE.questions[STORE.currentQuestion].answers;
-  let answers = "";
+  const answersArray = STORE.questions[STORE.currentQuestion].answers
+  let answers = '';
   let i = 0;
 
-  answersArray.forEach((answer) => {
+  answersArray.forEach(answer => {
     answers += `
     <div id="answer-option-${i}">
-      <input type="radio" name="option" id="option${
-        i + 1
-      }" value="${answer}" tabindex="${i + 1}" required>
+      <input type="radio" name="option" id="option${i + 1}" value="${answer}" tabindex="${i + 1}" required>
       <label for="option${i + 1}">${answer}</label>
       </div>
     `;
@@ -123,16 +121,16 @@ function generateQuestion() {
   return `
     <form id="question-form">
       <fieldset>
-        <div class="question">
-          <legend> ${currentQuestion.question}</legend>
+        <div class="question" class="quesiton-form">
+          <legend>${currentQuestion.question}</legend>
         </div>
         <div class="options">
-          <div class="answers>
-            ${generateAnswers()};
+          <div class="answers">
+            ${generateAnswers()}
           </div>
         </div>
         <button type="submit" id="submit-answer-button" tabindex="5">Submit</button>
-        <button type="button" id="next-question-button" tabindex="6">Next Question</button>
+        <button type="button" id="next-question-button" tabindex="6">Next</button>
       </fieldset>
     </form>
   `;
@@ -164,11 +162,11 @@ function generateFeedback (answerStatus) {
   let html = '';
   if (answerStatus === 'correct') {
     html = `
-    <p class="right-answer">Correct!</p>
+    <div class="right-answer">Correct!</div>
     `;
   } else if (answerStatus === 'incorrect') {
     html = `
-    <p class="wrong-answer">Incorrect. The right answer is ${correctAnswer}</p>
+    <div class="wrong-answer">Incorrect. The right answer is ${correctAnswer}</div>
     `;
   }
   return html;
@@ -181,18 +179,79 @@ function generateFeedback (answerStatus) {
 function render() {
   let html = '';
 
-  if(STORE.quizStarted === false) {
+  if (STORE.quizStarted === false) {
     $('main').html(generateQuizStartScreen());
     return;
   } else if (STORE.currentQuestion >= 0 && STORE.currentQuestion < STORE.questions.length) {
     html = generateQuestionNumberAndScore();
-    html += generateQuestionNumberAndScore();
+    html += generateQuestion();
     $('main').html(html);
   } else {
-    $('main').html(generateResultsScreen());
+    $('main').html(generateResults());
   }
 }
 
 /********** EVENT HANDLER FUNCTIONS **********/
 
 // These functions handle events (submit, click, etc)
+
+function handleStartClick () {
+  $('main').on('click', '#startButton', (event) => {
+    STORE.quizStarted = true;
+    render();
+  });
+}
+
+function handleNextQuestionClick () {
+  $('body').on('click', '#next-question-button', (event) => {
+    render();
+  });
+}
+
+function handleQuestionFormSubmission() {
+  $('body').on('submit', '#question-form', function (event) {
+    event.preventDefault();
+    const currentQuestion = STORE.questions[STORE.currentQuestion];
+
+    let selectedOption = $('input[name=option]:checked').val();
+
+    let optionContainerId = `#answer-option-${currentQuestion.answers.findIndex(i => i === selectedOption)}`;
+    
+    if (selectedOption === currentQuestion.correctAnswer) {
+      STORE.score++;
+      $(optionContainerId).append(generateFeedback('correct'));
+    } else {
+      $(optionContainerId).append(generateFeedback('incorrect'));
+    }
+    STORE.currentQuestion++;
+    $('#submit-answer-button').hide();
+    $('input[type=radio]').each(() => {
+      $('input[type=radio]').attr('disabled', true);
+    });
+    $('#next-question-button').show();
+  });
+}
+
+function restartQuiz() {
+  STORE.quizstarted = false;
+  STORE.currentQuestion = 0;
+  STORE.score = 0;
+}
+
+function handleRestartButtonClick() {
+  $('body').on('click', '#restart', () => {
+    restartQuiz();
+    render();
+  });
+}
+
+function handleQuizApp () {
+  render();
+  handleStartClick();
+  handleNextQuestionClick();
+  handleQuestionFormSubmission();
+  handleRestartButtonClick();
+}
+
+$(handleQuizApp);
+
